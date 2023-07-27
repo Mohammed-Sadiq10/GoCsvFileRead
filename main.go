4f7128c6b0037d85c7c44a53d672ed9bb5eff3d4
Mohammed Sadiq <mohammed_sadiq@thbs.india.com>
2023-07-27 15:14:27 +0530

    chages in output made

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	csvRead "github.com/Mohammed-Sadiq10/GoCsvFileRead/CSV"
 	jsonOutput "github.com/Mohammed-Sadiq10/GoCsvFileRead/JSON"
-	"fmt"
+	//"fmt"
 	"log"
 )
 
@@ -16,13 +16,13 @@ func main() {
 
 	log.Println("Processed Employee Data:")
 	for _, emp := range employees {
-		fmt.Printf("\nID: %d, Name: %s %s, Age: %d, Salary: %.2f, IsActive: %v\n", emp.ID, emp.FirstName, emp.LastName, emp.Age, emp.Salary, emp.IsActive)
+		log.Printf("ID: %d, Name: %s %s, Email: %s, Phone: %s, Age: %d, Salary: %.2f, IsActive: %v", emp.ID, emp.FirstName, emp.LastName, emp.Email, emp.Phone, emp.Age, emp.Salary, emp.IsActive)
 	}
 
-	err = jsonOutput.WriteJSONFile("output.json", employees)
+	err = jsonOutput.WriteJSONFile("jsonOutput.json", employees)
 	if err != nil {
 		log.Fatal("Error writing JSON data:", err)
 	}
 
-	log.Println("JSON data has been written to output.json")
+	log.Println("JSON data has been written to jsonOutput.json")
 }
